d1731557e661c7c31d9379e854a07b51f859657b
Arijit Patra <ari.clicks@gmail.com>
2019-03-23 14:46:04 +0530

    functions added

--- src/components/campaigns/campaigns.js
+++ src/components/campaigns/campaigns.js
@@ -10,19 +10,52 @@ class Campaigns extends Component {
           id: 1,
           name: "Emails",
           createdAt: "2:30 pm",
-          createdBy: "Chirag"
+          createdBy: "Chirag",
+          isRenamed: false,
+          isPaused: false,
+          previousName: "",
+          activity: [
+            {
+              code: 0,
+              type: "created",
+              activityBy: "userName",
+              dateTime: new Date()
+            }
+          ]
         },
         {
           id: 2,
           name: "Push notification",
           createdAt: "2:30 pm",
-          createdBy: "Chirag"
+          createdBy: "Chirag",
+          isRenamed: false,
+          isPaused: false,
+          previousName: "",
+          activity: [
+            {
+              code: 0,
+              type: "created",
+              activityBy: "userName",
+              dateTime: new Date()
+            }
+          ]
         },
         {
           id: 3,
           name: "InApp Messages",
           createdAt: "2:30 pm",
-          createdBy: "Chirag"
+          createdBy: "Chirag",
+          isRenamed: false,
+          isPaused: false,
+          previousName: "",
+          activity: [
+            {
+              code: 0,
+              type: "created",
+              activityBy: "userName",
+              dateTime: new Date()
+            }
+          ]
         }
       ],
       initial: ""
@@ -37,11 +70,82 @@ class Campaigns extends Component {
           id: this.state.data.length + 1,
           name: "InApp Messages",
           createdAt: "2:30 pm",
-          createdBy: "Chirag"
+          createdBy: "Chirag",
+          isRenamed: false,
+          isPaused: false,
+          previousName: "",
+          activity: [
+            {
+              code: 0,
+              type: "created",
+              activityBy: "userName",
+              dateTime: new Date()
+            }
+          ]
         }
       ]
     });
   }
+
+  onResume = id => {
+    let dataCopy = JSON.parse(JSON.stringify(this.state.data));
+    dataCopy[id - 1].isPaused = false;
+    dataCopy[id - 1].activity.push({
+      code: 5,
+      type: "resume",
+      activityBy: "userName",
+      dateTime: new Date()
+    });
+    this.setState({ data: dataCopy });
+  };
+
+  onPause = id => {
+    let dataCopy = JSON.parse(JSON.stringify(this.state.data));
+    dataCopy[id - 1].isPaused = true;
+    dataCopy[id - 1].activity.push({
+      code: 1,
+      type: "pause",
+      activityBy: "userName",
+      dateTime: new Date()
+    });
+    this.setState({ data: dataCopy });
+  };
+
+  onComment = id => {
+    let dataCopy = JSON.parse(JSON.stringify(this.state.data));
+    dataCopy[id - 1].activity.push({
+      code: 2,
+      type: "comment",
+      comment: "this is a sample comment",
+      activityBy: "userName",
+      dateTime: new Date()
+    });
+    this.setState({ data: dataCopy });
+  };
+
+  onRename = id => {
+    let dataCopy = JSON.parse(JSON.stringify(this.state.data));
+    dataCopy[id - 1].previousName = dataCopy[id - 1].name;
+    dataCopy[id - 1].name = "Rename";
+    dataCopy[id - 1].isRenamed = true;
+    dataCopy[id - 1].activity.push({
+      code: 3,
+      type: "rename",
+      activityBy: "userName",
+      dateTime: new Date()
+    });
+    this.setState({ data: dataCopy });
+  };
+
+  onDelete = id => {
+    this.setState({
+      data: this.state.data.filter(x => {
+        if (x.id !== id) {
+          return x;
+        }
+      })
+    });
+  };
 
   render() {
     return (
@@ -64,10 +168,39 @@ class Campaigns extends Component {
             </div>
 
             <div className="col-4">
-              <i className="fa fa-pause-circle-o mr-5" aria-hidden="true" />
-              <i className="fa fa-comments-o mr-5" aria-hidden="true" />
-              <i className="fa fa-pencil mr-5" aria-hidden="true" />
-              <i className="fa fa-trash" aria-hidden="true" />
+              {x.isPaused === false ? (
+                <i
+                  className="fa fa-pause-circle-o mr-5"
+                  title="Pause"
+                  aria-hidden="true"
+                  onClick={() => this.onPause(x.id)}
+                />
+              ) : (
+                <i
+                  className="fa fa-play-circle-o mr-5"
+                  title="Resume"
+                  aria-hidden="true"
+                  onClick={() => this.onResume(x.id)}
+                />
+              )}
+              <i
+                className="fa fa-comments-o mr-5"
+                title="Comment"
+                aria-hidden="true"
+                onClick={() => this.onComment(x.id)}
+              />
+              <i
+                className="fa fa-pencil mr-5"
+                title="Rename"
+                aria-hidden="true"
+                onClick={() => this.onRename(x.id)}
+              />
+              <i
+                className="fa fa-trash"
+                title="Delete"
+                aria-hidden="true"
+                onClick={() => this.onDelete(x.id)}
+              />
             </div>
           </div>
         ))}
